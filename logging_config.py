4b1af93f898b8fd17bc1b64c4d4f47966cd3783f
lgoncalves94 <goncalves.leonardo@proton.me>
2024-09-11 11:26:34 +0200

    added further instructions

--- logging_config.py
+++ logging_config.py
@@ -1,11 +1,11 @@
 import logging
 
-# Configure the logging settings
+# Configure the logging settings - Default Setting is Warning-Level and no file log
 logging.basicConfig(
     level=logging.WARNING,  # Adjust the log level as needed (DEBUG, INFO, WARNING, ERROR, CRITICAL)
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
-        #logging.FileHandler('data_exploration.log'),   <- Uncomment to implement File logging
+        #logging.FileHandler('data_clean.log'),   <- Uncomment to implement File logging
         logging.StreamHandler()  # Also output to console
     ]
 )
