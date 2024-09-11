db499f3e45d6728999b87e75cd5f749ed69da29a
L.Goncalves <goncalves.leonardo@proton.me>
2024-09-11 11:23:07 +0200

    Update logging_config.py
    revised logging instructions

--- logging_config.py
+++ logging_config.py
@@ -5,7 +5,7 @@ logging.basicConfig(
     level=logging.WARNING,  # Adjust the log level as needed (DEBUG, INFO, WARNING, ERROR, CRITICAL)
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
-        #logging.FileHandler('data_exploration.log'),  # Log to a file
+        #logging.FileHandler('data_exploration.log'),   <- Uncomment to implement File logging
         logging.StreamHandler()  # Also output to console
     ]
 )
